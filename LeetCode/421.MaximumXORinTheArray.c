0368c806e2313bc07df06cafe0079715c5298ae0
Hariharasudhan <sudanayyappan_bcs28@mepcoeng.ac.in>
2026-07-29 14:59:42 +0530

    Update 421.MaximumXORinTheArray.c

--- LeetCode/421.MaximumXORinTheArray.c
+++ LeetCode/421.MaximumXORinTheArray.c
@@ -1,13 +1,53 @@
+typedef struct Node {
+    struct Node *child[2];
+} Node;
+
+Node* createNode() {
+    Node *node = malloc(sizeof(Node));
+    node->child[0] = NULL;
+    node->child[1] = NULL;
+    return node;
+}
+
+void insert(Node *root, int num) {
+    Node *cur = root;
+
+    for (int i = 31; i >= 0; i--) {
+        int bit = (num >> i) & 1;
+
+        if (cur->child[bit] == NULL)
+            cur->child[bit] = createNode();
+
+        cur = cur->child[bit];
+    }
+}
+
 int findMaximumXOR(int* nums, int numsSize) {
-    int max = 0;
+    Node *root = createNode();
+
+    for (int i = 0; i < numsSize; i++)
+        insert(root, nums[i]);
+
+    int ans = 0;
 
     for (int i = 0; i < numsSize; i++) {
-        for (int j = i + 1; j < numsSize; j++) {
-            int temp = nums[i] ^ nums[j];
-            if (temp > max)
-                max = temp;
+        Node *cur = root;
+        int value = 0;
+
+        for (int j = 31; j >= 0; j--) {
+            int bit = (nums[i] >> j) & 1;
+
+            if (cur->child[1 - bit]) {
+                value |= (1 << j);
+                cur = cur->child[1 - bit];
+            } else {
+                cur = cur->child[bit];
+            }
         }
+
+        if (value > ans)
+            ans = value;
     }
 
-    return max;
-};
+    return ans;
+}
